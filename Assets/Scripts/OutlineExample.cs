0252163213b4cf33fdd3d2e0af8ccc4b491d725a
Daniel Ilett <danielilett@gmail.com>
2020-04-24 01:42:17 +0100

    Added option for corner sampling.

---
+++ Assets/Scripts/OutlineExample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineExample : MonoBehaviour
+{
+    private Material material;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        var size = Mathf.Sin(Time.time) + 1.0f;
+        material.SetFloat("_OutlineThickness", size);
+    }
+}
